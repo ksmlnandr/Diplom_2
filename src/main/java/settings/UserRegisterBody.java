d58414d0ff200b95023514f5036892c875acfdd6
Andrei Kosmylin <kosmylin-andrei@yandex.ru>
2024-09-08 17:25:48 +0300

    в UserRegisterTest создан тест на регистрацию нового пользователя

---
+++ src/main/java/settings/UserRegisterBody.java
@@ -0,0 +1,39 @@
+package settings;
+
+public class UserRegisterBody extends UserLoginBody {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String getEmail() {
+        return super.getEmail();
+    }
+
+    @Override
+    public void setEmail(String email) {
+        super.setEmail(email);
+    }
+
+    @Override
+    public String getPassword() {
+        return super.getPassword();
+    }
+
+    @Override
+    public void setPassword(String password) {
+        super.setPassword(password);
+    }
+
+    public UserRegisterBody(String email, String password, String name) {
+        super(email, password);
+        this.name = name;
+    }
+}
--- src/test/java/UserRegisterTest.java
+++ src/test/java/UserRegisterTest.java
@@ -2,16 +2,21 @@ import io.qameta.allure.Step;
 import io.qameta.allure.junit4.DisplayName;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import settings.RestClient;
+import settings.UserRegisterBody;
+
 import static org.hamcrest.Matchers.*;
 
 
 import static io.restassured.RestAssured.given;
 
 public class UserRegisterTest {
-    private RestClient restClient = new RestClient();
+    private final RestClient restClient = new RestClient();
+    private UserRegisterBody body;
+    private Response response;
 
     @Before
     public void setUp() {
@@ -19,35 +24,77 @@ public class UserRegisterTest {
     }
 
     @Test
-    @DisplayName("Регистрация нового пользователя")
+    @DisplayName("Тест регистрации нового пользователя")
     public void newUserRegisterTest() {
-        UserRegisterBody body = setRequestBody("новый пользователь");
-        Response response = getUserRegister(body);
-        checkResponseBody(response);
+        body = setRequestBody("новый пользователь");
+        response = getUserRegister(body);
+        checkValidResponseBody(response);
+        checkStatusCode(response, 200);
     }
 
+    @Test
+    @DisplayName("Тест регистрации существующего пользователя")
+    public void existingUserRegisterTest() {
+        body = setRequestBody("существующий пользователь");
+        response = getUserRegister(body);
+        checkInvalidResponseBody(response, body);
+        checkStatusCode(response, 403);
+    }
+
+    @Test
+    @DisplayName("Тест регистрации нового пользователя без email")
+    public void noEmailRegisterTest() {
+        body = setRequestBody("пользователь без email");
+        response = getUserRegister(body);
+        checkInvalidResponseBody(response, body);
+        checkStatusCode(response, 403);
+    }
+
+    @Test
+    @DisplayName("Тест регистрации нового пользователя без пароля")
+    public void noPasswordRegisterTest() {
+        body = setRequestBody("пользователь без пароля");
+        response = getUserRegister(body);
+        checkInvalidResponseBody(response, body);
+        checkStatusCode(response, 403);
+    }
+
+    @Test
+    @DisplayName("Тест регистрации нового пользователя без без имени")
+    public void noNameRegisterTest() {
+        body = setRequestBody("пользователь без имени");
+        response = getUserRegister(body);
+        checkInvalidResponseBody(response, body);
+        checkStatusCode(response, 403);
+    }
+
+
+    @After
+    public void cleanUp() {
+        //дописать метод удаления данных пользователя
+    }
 
 
     @Step("Создано тело запроса")
     public UserRegisterBody setRequestBody(String bodyOption){
-        UserRegisterBody newUser = null;
+        body = null;
         if(bodyOption == "новый пользователь") {
-            newUser = new UserRegisterBody("new@user.ru", "12345", "New User");
+            body = new UserRegisterBody("new@user.ru", "12345", "New User");
         } else if (bodyOption == "существующий пользователь") {
-            newUser = new UserRegisterBody("existing@user.ru", "123456", "Existing User");
+            body = new UserRegisterBody("existing@user.ru", "123456", "Existing User");
         } else if (bodyOption == "пользователь без email") {
-            newUser = new UserRegisterBody(null, "123456", "NoEmail User");
+            body = new UserRegisterBody(null, "123456", "NoEmail User");
         } else if (bodyOption == "пользователь без пароля") {
-            newUser = new UserRegisterBody("nopassword@user.ru", null, "NoPassword User");
+            body = new UserRegisterBody("nopassword@user.ru", null, "NoPassword User");
         } else if (bodyOption == "пользователь без имени") {
-            newUser = new UserRegisterBody("noname@user.ru", "1234567", null);
+            body = new UserRegisterBody("noname@user.ru", "123456", null);
         }
-        return newUser;
+        return body;
     }
 
     @Step("Вызван метод регистрации нового пользователя")
     public Response getUserRegister(UserRegisterBody body) {
-        Response response =
+        response =
                 given()
                         .header("Content-type", "application/json")
                         .and()
@@ -63,14 +110,30 @@ public class UserRegisterTest {
     }
 
     @Step("Получено ожидаемое тело ответа при успешном создании нового пользователя")
-    public void checkResponseBody(Response response) {
-        UserRegisterBody body = setRequestBody("новый пользователь");
-
+    public void checkValidResponseBody(Response response) {
         response.then().assertThat()
                 .body("success", equalTo(true))
                 .body("user.email", equalTo(body.getEmail()))
                 .body("user.name", equalTo(body.getName()))
                 .body("accessToken", contains("Bearer"))
                 .body("refreshToken", equalTo(String.class));
+    }
+
+    @Step("Получено ожидаемое тело ответа при попытке регистрации пользователя с некорректными данными")
+    public void checkInvalidResponseBody(Response response, UserRegisterBody body) {
+        if(body.getEmail().equals(notNullValue())
+                && body.getPassword().equals(notNullValue())
+                && body.getName().equals(notNullValue())) {
+            response.then().assertThat()
+                    .body("success", equalTo(false))
+                    .body("message", equalTo("User already exists"));
+        } else
+            if (body.getEmail().equals(null)
+                || body.getPassword().equals(null)
+                || body.getName().equals(null)) {
+            response.then().assertThat()
+                    .body("success", equalTo(false))
+                    .body("message", equalTo("Email, password and name are required fields"));
+        }
     }
 }
