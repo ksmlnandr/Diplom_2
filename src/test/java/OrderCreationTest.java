c835caf748d7be683cac48e8072fdd7cb50c1ee0
Andrei Kosmylin <kosmylin-andrei@yandex.ru>
2024-09-14 22:22:33 +0300

    доработаны тесты на оформление заказа

--- src/test/java/OrderCreationTest.java
+++ src/test/java/OrderCreationTest.java
@@ -1,28 +1,22 @@
-import io.qameta.allure.Step;
 import io.qameta.allure.junit4.DisplayName;
-import io.restassured.RestAssured;
 import io.restassured.response.Response;
+import model.CommonMethods;
+import model.Steps;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import settings.*;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
-import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.*;
-
 public class OrderCreationTest {
-    private RestClient restClient = new RestClient();
+    private CommonMethods commonMethods = new CommonMethods();
+    private Steps step = new Steps();
     private Response response;
-    private UserRegisterBody urb = new UserRegisterBody("new3@user.ru", "12345", "New User");
-    private UserRegisterTest urt = new UserRegisterTest();
-    private UserResponseBody responseBody;
-    private UserDeleteApi uda = new UserDeleteApi();
+    private UserRegisterBody regBody = new UserRegisterBody("new3@user.ru", "12345", "New User");
     private String accessToken;
-    private OrderCreateBody ocb;
+    private OrderCreateBody orderCreateBody;
     private List<OrderCreateBody> orderCreateBodies = new ArrayList<OrderCreateBody>();
     public OrderCreationTest() {
         orderCreateBodies.add(new OrderCreateBody(new String[] {"61c0c5a71d1f82001bdaaa6d", "61c0c5a71d1f82001bdaaa70"}));
@@ -32,127 +26,57 @@ public class OrderCreationTest {
 
     @Before
     public void setUp() {
-        RestAssured.baseURI = restClient.getBaseUrl();
+        commonMethods.setBaseUrl();
 
-        response = urt.getUserRegister(urb);
-        responseBody = response.body().as(UserResponseBody.class);
-        accessToken = responseBody.getAccessToken();
+        response = step.getUserRegister(regBody);
+        accessToken = step.getAccessToken(response);
     }
 
     @Test
     @DisplayName("Тест создания заказа с авторизацией")
     public void createOrderWithAuthTest() {
-        ocb = orderCreateBodies.get(0);
-        response = postCreateOrderWithAuth(ocb, accessToken);
-        checkStatusCode(response, 200);
-        checkValidResponseBodyWithAuth(response);
+        orderCreateBody = orderCreateBodies.get(0);
+        response = step.postCreateOrderWithAuth(orderCreateBody, accessToken);
+        step.checkStatusCode(response, 200);
+        step.checkOrderValidResponseBodyWithAuth(response);
     }
 
     @Test
     @DisplayName("Тест создания заказа без авторизации")
     public void createOrderWithoutAuthTest() {
-        ocb = orderCreateBodies.get(0);
-        response = postCreateOrderWithoutAuth(ocb);
-        checkStatusCode(response, 401);
+        orderCreateBody = orderCreateBodies.get(0);
+        response = step.postCreateOrderWithoutAuth(orderCreateBody);
+        step.checkStatusCode(response, 401);
     }
 
     @Test
     @DisplayName("Тест создания заказа с ингредиентами")
     public void createOrderWithIngredientsTest() {
-        ocb = orderCreateBodies.get(0);
-        response = postCreateOrderWithAuth(ocb, accessToken);
-        checkStatusCode(response, 200);
-        checkValidResponseBodyWithAuth(response);
+        orderCreateBody = orderCreateBodies.get(0);
+        response = step.postCreateOrderWithAuth(orderCreateBody, accessToken);
+        step.checkStatusCode(response, 200);
+        step.checkOrderValidResponseBodyWithAuth(response);
     }
 
     @Test
     @DisplayName("Тест создания заказа без ингредиентов")
     public void createOrderWithoutIngredientsTest() {
-        ocb = orderCreateBodies.get(1);
-        response = postCreateOrderWithAuth(ocb, accessToken);
-        checkStatusCode(response, 400);
-        checkInvalidResponseBodyWithoutIngredients(response);
+        orderCreateBody = orderCreateBodies.get(1);
+        response = step.postCreateOrderWithAuth(orderCreateBody, accessToken);
+        step.checkStatusCode(response, 400);
+        step.checkOrderInvalidResponseBodyWithoutIngredients(response);
     }
 
     @Test
     @DisplayName("Тест создания заказа с неверным хэшем ингредиентов")
     public void createOrderWithInvalidIngredientsTest() {
-        ocb = orderCreateBodies.get(2);
-        response = postCreateOrderWithAuth(ocb, accessToken);
-        checkStatusCode(response, 500);
+        orderCreateBody = orderCreateBodies.get(2);
+        response = step.postCreateOrderWithAuth(orderCreateBody, accessToken);
+        step.checkStatusCode(response, 500);
     }
 
     @After
     public void cleanUp() {
-        uda.cleanUp(accessToken);
-    }
-
-    @Step("Вызван метод создания заказа с авторизацией")
-    public Response postCreateOrderWithAuth(OrderCreateBody ocb, String accessToken) {
-        response =
-                given()
-                        .header("Content-type", "application/json")
-                        .and()
-                        .header("Authorization", accessToken)
-                        .and()
-                        .body(ocb)
-                        .when()
-                        .post(restClient.getOrdersEndpoint());
-        return response;
-    }
-
-    @Step("Вызван метод создания заказа без авторизации")
-    public Response postCreateOrderWithoutAuth(OrderCreateBody ocb) {
-        response =
-                given()
-                        .header("Content-type", "application/json")
-                        .and()
-                        .body(ocb)
-                        .when()
-                        .post(restClient.getOrdersEndpoint());
-        return response;
-    }
-
-    @Step("В ответе получен ожидаемый статус-код")
-    public void checkStatusCode(Response response, int statusCode) {
-        response.then().statusCode(statusCode);
-    }
-
-
-    @Step("Получено ожидаемое тело ответа при успешном создании заказа без авторизации")
-    public void checkValidResponseBodyWithAuth(Response response) {
-        response.then().assertThat()
-                .body("success", equalTo(true))
-                .body("name", instanceOf(String.class))
-                .body("order.ingredients._id", instanceOf(ArrayList.class))
-                .body("order.ingredients.name", instanceOf(ArrayList.class))
-                .body("order.ingredients.type", instanceOf(ArrayList.class))
-                .body("order.ingredients.proteins", instanceOf(ArrayList.class))
-                .body("order.ingredients.fat", instanceOf(ArrayList.class))
-                .body("order.ingredients.carbohydrates", instanceOf(ArrayList.class))
-                .body("order.ingredients.calories", instanceOf(ArrayList.class))
-                .body("order.ingredients.price", instanceOf(ArrayList.class))
-                .body("order.ingredients.image", instanceOf(ArrayList.class))
-                .body("order.ingredients.image_mobile", instanceOf(ArrayList.class))
-                .body("order.ingredients.image_large", instanceOf(ArrayList.class))
-                .body("order.ingredients.__v", instanceOf(ArrayList.class))
-                .body("order._id", instanceOf(String.class))
-                .body("order.owner.name", instanceOf(String.class))
-                .body("order.owner.email", instanceOf(String.class))
-                .body("order.owner.createdAt", instanceOf(String.class))
-                .body("order.owner.updatedAt", instanceOf(String.class))
-                .body("order.status", instanceOf(String.class))
-                .body("order.name", instanceOf(String.class))
-                .body("order.createdAt", instanceOf(String.class))
-                .body("order.updatedAt", instanceOf(String.class))
-                .body("order.number", instanceOf(Integer.class))
-                .body("order.price", instanceOf(Integer.class));
-    }
-
-    @Step("Получено ожидаемое тело ответа при попытке создания заказа без ингредиентов")
-    public void checkInvalidResponseBodyWithoutIngredients(Response response) {
-        response.then().assertThat()
-                .body("success", equalTo(false))
-                .body("message", equalTo("Ingredient ids must be provided"));
+        commonMethods.cleanUp(accessToken);
     }
 }
--- src/test/java/model/Steps.java
+++ src/test/java/model/Steps.java
@@ -3,9 +3,12 @@ package model;
 
 import io.qameta.allure.Step;
 import io.restassured.response.Response;
+import settings.OrderCreateBody;
 import settings.UserLoginBody;
 import settings.UserRegisterBody;
 import settings.UserResponseBody;
+
+import java.util.ArrayList;
 
 import static settings.RestClient.*;
 import static io.restassured.RestAssured.given;
@@ -103,5 +106,69 @@ public class Steps {
         response.then().assertThat()
                 .body("success", equalTo(false))
                 .body("message", equalTo("email or password are incorrect"));
+    }
+
+    //шаги для тестов на оформление нового заказа
+    @Step("Вызван метод создания заказа с авторизацией")
+    public Response postCreateOrderWithAuth(OrderCreateBody orderCreateBody, String accessToken) {
+        response =
+                given()
+                        .header("Content-type", "application/json")
+                        .and()
+                        .header("Authorization", accessToken)
+                        .and()
+                        .body(orderCreateBody)
+                        .when()
+                        .post(ORDERS_ENDPOINT);
+        return response;
+    }
+
+    @Step("Вызван метод создания заказа без авторизации")
+    public Response postCreateOrderWithoutAuth(OrderCreateBody ocb) {
+        response =
+                given()
+                        .header("Content-type", "application/json")
+                        .and()
+                        .body(ocb)
+                        .when()
+                        .post(ORDERS_ENDPOINT);
+        return response;
+    }
+
+    @Step("Получено ожидаемое тело ответа при успешном создании заказа без авторизации")
+    public void checkOrderValidResponseBodyWithAuth(Response response) {
+        response.then().assertThat()
+                .body("success", equalTo(true))
+                .body("name", instanceOf(String.class))
+                .body("order.ingredients._id", instanceOf(ArrayList.class))
+                .body("order.ingredients.name", instanceOf(ArrayList.class))
+                .body("order.ingredients.type", instanceOf(ArrayList.class))
+                .body("order.ingredients.proteins", instanceOf(ArrayList.class))
+                .body("order.ingredients.fat", instanceOf(ArrayList.class))
+                .body("order.ingredients.carbohydrates", instanceOf(ArrayList.class))
+                .body("order.ingredients.calories", instanceOf(ArrayList.class))
+                .body("order.ingredients.price", instanceOf(ArrayList.class))
+                .body("order.ingredients.image", instanceOf(ArrayList.class))
+                .body("order.ingredients.image_mobile", instanceOf(ArrayList.class))
+                .body("order.ingredients.image_large", instanceOf(ArrayList.class))
+                .body("order.ingredients.__v", instanceOf(ArrayList.class))
+                .body("order._id", instanceOf(String.class))
+                .body("order.owner.name", instanceOf(String.class))
+                .body("order.owner.email", instanceOf(String.class))
+                .body("order.owner.createdAt", instanceOf(String.class))
+                .body("order.owner.updatedAt", instanceOf(String.class))
+                .body("order.status", instanceOf(String.class))
+                .body("order.name", instanceOf(String.class))
+                .body("order.createdAt", instanceOf(String.class))
+                .body("order.updatedAt", instanceOf(String.class))
+                .body("order.number", instanceOf(Integer.class))
+                .body("order.price", instanceOf(Integer.class));
+    }
+
+    @Step("Получено ожидаемое тело ответа при попытке создания заказа без ингредиентов")
+    public void checkOrderInvalidResponseBodyWithoutIngredients(Response response) {
+        response.then().assertThat()
+                .body("success", equalTo(false))
+                .body("message", equalTo("Ingredient ids must be provided"));
     }
 }
